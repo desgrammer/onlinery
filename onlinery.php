112135df2b8d67b41222912997b8e1a401670ed2
Junnaedi <muhammad_junaidi@live.com>
2021-01-03 11:53:55 +0700

    change metadata

--- onlinery.php
+++ onlinery.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       Onlinery
  * Plugin URI:        https://onlinery.id
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Description:       The most versatile Online Store plugin.
+ * Version:           0.0.1
  * Author:            DesGrammer
  * Author URI:        https://desgrammer.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ONLINERY_VERSION', '1.0.0' );
+define( 'ONLINERY_VERSION', '0.0.1' );
 
 /**
  * The code that runs during plugin activation.
